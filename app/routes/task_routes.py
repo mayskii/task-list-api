a63c89d38da31c1d80ccda753e6c301e41280454
Maya Zakharova <mayzakharova@gmail.com>
2025-10-30 17:48:37 -0500

    Register Blueprint for tasks and add first route for post one task

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, abort, request, Response
+from app.models.task import Task
+from ..db import db
+
+bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
+
+    try:
+        new_task = Task.from_dict(request_body)
+
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(new_task.to_dict(), 201)
