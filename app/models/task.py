fa10b6174268f5baf11b7f234ef6b7587d0740ed
mayskii <94030074+mayskii@users.noreply.github.com>
2025-10-30 22:10:54 -0500

    Wave 1 (#1)
    * Add Task model and migration for  Wave 1
    
    * Register Blueprint for tasks and add first route for post one task
    
    * Add method for get all and one task and validation function
    
    * Add method for update and delete one task
    
    * Fix typo

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,27 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 from ..db import db
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[datetime | None] = mapped_column(nullable=True)
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = cls(
+            title = task_data["title"],
+            description = task_data["description"],
+            completed_at = task_data.get("completed_at")
+        )
+        return new_task
+    
+    def to_dict(self):
+        task_as_dict = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at is not None
+        }
+        return task_as_dict
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,70 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, abort, request, Response
+from app.models.task import Task
+from .route_utilities import validate_model
+from ..db import db
+
+bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+
+# POST one task
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
+
+    try:
+        new_task = Task.from_dict(request_body)
+
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(new_task.to_dict(), 201)
+
+# GET all tasks
+@bp.get("")
+def get_all_tasks():
+
+    query = db.select(Task)
+
+    #sorting will be here
+    query = query.order_by(Task.id)
+    tasks = db.session.scalars(query)
+
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(task.to_dict())
+    return tasks_response
+
+# GET obe task
+@bp.get("/<task_id>")
+def get_one_task(task_id):
+    task = validate_model(Task, task_id)
+
+    return make_response(task.to_dict(), 200)
+
+# PUT one task
+@bp.put("/<task_id>")
+def update_one_task(task_id):
+    task = validate_model(Task, task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    task.completed_at = request_body.get("completed_at", None)
+
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
+
+
+# DELETE one task
+@bp.delete("/<task_id>")
+def delete_one_task(task_id):
+    task = validate_model(Task, task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
