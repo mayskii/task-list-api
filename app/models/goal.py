7399f13ab8ac8e5c3964d1e5c63132fb4d23d288
Maya Zakharova <mayzakharova@gmail.com>
2025-11-02 16:30:51 -0600

    add Goal model and migrations

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,18 @@ from ..db import db
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+
+    @classmethod
+    def from_dict(cls, goal_data):
+        new_goal = cls(
+            title = goal_data["title"]
+        )
+        return new_goal
+    
+    def to_dict(self):
+        goal_as_dict = {
+            "id": self.id,
+            "title": self.title
+        }
+        return  goal_as_dict
