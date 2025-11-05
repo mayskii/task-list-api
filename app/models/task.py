11021664c74a29219cc1ec4652a691b3d677bb4a
mayskii <94030074+mayskii@users.noreply.github.com>
2025-11-04 22:49:03 -0600

    Wave 6 (#7)
    * update model for Task and Goal
    
    * add post route fot connected with goal tasks
    
    * add get route for connected with goal tasks
    
    * add create_model function in route_utilities

--- app/models/task.py
+++ app/models/task.py
@@ -1,18 +1,30 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
+from sqlalchemy import ForeignKey
+from typing import Optional
 from ..db import db
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .goal import Goal
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
     completed_at: Mapped[datetime | None] = mapped_column(nullable=True)
 
     @classmethod
     def from_dict(cls, task_data):
+
+        goal_id = task_data.get("goal_id")
+
         new_task = cls(
             title = task_data["title"],
             description = task_data["description"],
+            goal_id = goal_id,
             completed_at = task_data.get("completed_at")
         )
         return new_task
@@ -24,4 +36,8 @@ class Task(db.Model):
             "description": self.description,
             "is_complete": self.completed_at is not None
         }
+
+        if self.goal_id:
+            task_as_dict["goal_id"] = self.goal_id
+
         return task_as_dict
--- app/routes/goal_routes.py
+++ app/routes/goal_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, make_response, abort, request, Response
 from app.models.goal import Goal
+from app.models.task import Task
 from .route_utilities import validate_model
 from datetime import datetime
 from ..db import db
@@ -73,3 +74,36 @@ def delete_one_goal(goal_id):
     db.session.commit()
 
     return Response(status=204, mimetype="application/json")
+
+# CREATE connected tasks to goal
+@bp.post("/<goal_id>/tasks")
+def assign_tasks_to_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+    request_body = request.get_json()
+    task_ids = request_body.get("task_ids", [])
+
+    tasks = []
+    for task_id in task_ids:
+        task = validate_model(Task, task_id)
+        tasks.append(task)
+    goal.tasks = tasks
+    
+    db.session.commit()
+
+    return make_response({"id": goal.id, "task_ids": task_ids}, 200)
+
+# GET tasks for goal
+@bp.get("/<goal_id>/tasks")
+def get_tasks_for_goal(goal_id):
+    goal = validate_model(Goal, goal_id)
+
+    tasks_response = [task.to_dict() for task in goal.tasks]
+
+    response_body = {
+        "id": goal.id,
+        "title": goal.title,
+        "tasks": tasks_response
+    }
+
+    return make_response(response_body, 200)
+
